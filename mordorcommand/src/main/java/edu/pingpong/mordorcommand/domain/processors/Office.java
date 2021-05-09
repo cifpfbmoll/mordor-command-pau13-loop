4e000b381f4181bf8b5e03048d0c3963d95a781a
pau <pllinas@cifpfbmoll.eu>
2021-05-09 20:21:04 +0200

    feat: :sparkles: Implement office and string formatter output

--- mordorcommand/src/main/java/edu/pingpong/mordorcommand/domain/processors/Office.java
+++ mordorcommand/src/main/java/edu/pingpong/mordorcommand/domain/processors/Office.java
@@ -1,5 +1,7 @@
 package edu.pingpong.mordorcommand.domain.processors;
 
+import java.util.Formatter;
+
 import edu.pingpong.mordorcommand.domain.interfaces.Order;
 import edu.pingpong.mordorcommand.domain.interfaces.OrderTreatment;
 import edu.pingpong.mordorcommand.domain.interfaces.Processor;
@@ -14,9 +16,28 @@ public class Office implements Processor{
     }
 
     public String printStatus(boolean status, Order order) {
-        return status ? 
-            "The order with a destination to " + order.destination() + " and a weight of " + order.weight() + " has been: " + Status.ACCEPTED.name()
-        :
-        "The order with a destination to " + order.destination() + " and a weight of " + order.weight() + " has been: " + Status.REJECTED.name();
+        StringBuilder sb = new StringBuilder();
+        Formatter formatter = new Formatter(sb);
+
+        return status ?
+
+                    formatter.format("%1$s %2$s", order.destination(), Status.ACCEPTED.name()).toString() :
+                    formatter.format("%1$s %2$s", order.destination(), Status.REJECTED.name()).toString();
+
+        // formatter.format("\nThe order with a destination to %1$s and a weight of %2$s has been: %3$s", order.destination(), order.weight(), Status.ACCEPTED.name()).toString()
+        // :
+
+        // formatter.format("\nThe order with a destination to %1$s and a weight of %2$s has been: %3$s", order.destination(), order.weight(), Status.REJECTED.name()).toString();
+
+        // String accepted = formatter.format("\nThe order with a destination to %1$s and a weight of %2$s has been: %3$s", order.destination(), order.weight(), Status.ACCEPTED.name()).toString();
+
+        // String rejected = formatter.format("\nThe order with a destination to %1$s and a weight of %2$s has been: %3$s", order.destination(), order.weight(), Status.REJECTED.name()).toString();
+
+        // formatter.close();
+
+        // return status ? accepted : rejected;
+        // return status? 
+        // order.destination() + " " + Status.ACCEPTED.name():
+        // order.destination() + " " + Status.REJECTED.name();
     }
 }
