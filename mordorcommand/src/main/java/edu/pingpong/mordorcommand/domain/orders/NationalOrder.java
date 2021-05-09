04f69be65f2c5d36c257249b30a67a10da5eca9f
pau <pllinas@cifpfbmoll.eu>
2021-05-09 20:13:04 +0200

    feat: :sparkles: Add national order class

--- mordorcommand/src/main/java/edu/pingpong/mordorcommand/domain/orders/NationalOrder.java
+++ mordorcommand/src/main/java/edu/pingpong/mordorcommand/domain/orders/NationalOrder.java
@@ -1,5 +1,31 @@
 package edu.pingpong.mordorcommand.domain.orders;
 
-public class NationalOrder {
-    
+import java.util.UUID;
+
+import edu.pingpong.mordorcommand.domain.interfaces.Order;
+
+public class NationalOrder implements Order {
+
+    private final String id;
+    private String destination;
+    private int weight;
+
+    public NationalOrder(String destination, int weight) {
+
+        this.id = UUID.randomUUID().toString();
+        this.destination = destination;
+        this.weight = weight;
+    }
+
+    public int weight() {
+        return this.weight;
+    }
+
+    public String destination() {
+        return this.destination;
+    }
+
+    public String getId() {
+        return this.id;
+    }
 }
